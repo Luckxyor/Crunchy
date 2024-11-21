c54ff924e37232c2a838756da298463e5e5bac64
LUCAS EIAL BENEZRA <48313633@ort>
2024-11-21 14:00:45 -0300

    estoy aburrido

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -41,6 +41,14 @@ public class AccountController : Controller
     [HttpPost]
     public IActionResult AgregarUsuario(string Username, string Email, string Contraseña){
         BD.AgregarUsuario(Username,Email,Contraseña);
+        Usuario usuario=BD.InicioSesion(Email, Contraseña);
+        string perfilPredeterminada = usuario.Foto ?? "https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg";
+        string  headerPredeterminado = usuario.FotoHeader ?? "https://media.istockphoto.com/id/1128826884/es/vector/ning%C3%BAn-s%C3%ADmbolo-de-vector-de-imagen-falta-icono-disponible-no-hay-galer%C3%ADa-para-este-momento.jpg?s=612x612&w=0&k=20&c=9vnjI4XI3XQC0VHfuDePO7vNJE7WDM8uzQmZJ1SnQgk=";
+        HttpContext.Session.SetInt32("IdUsuario", usuario.IdUsuario);
+        HttpContext.Session.SetString("Email", usuario.Email);
+        HttpContext.Session.SetString("Username", usuario.Username);
+        HttpContext.Session.SetString("Foto", perfilPredeterminada);
+        HttpContext.Session.SetString("FotoHeader", headerPredeterminado);
         return RedirectToAction ("Home", "Home");
     }
 
